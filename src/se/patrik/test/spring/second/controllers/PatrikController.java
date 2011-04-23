9a62c91e634357e802ce244746b40ae67868fac3
Patrik Carlsson <filurpuc@gmail.com>
2011-04-23 21:48:33 +0200

    Injecting service. Tested models.

--- src/se/patrik/test/spring/second/controllers/PatrikController.java
+++ src/se/patrik/test/spring/second/controllers/PatrikController.java
@@ -1,21 +1,44 @@
 package se.patrik.test.spring.second.controllers;
 
+import java.util.Date;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import se.patrik.test.spring.second.services.PatrikService;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
-public class PatrikController {
+public class PatrikController
+{
 	private static Logger logger = Logger.getLogger(PatrikController.class);
-	
-	@RequestMapping({"/","/patrik"})
-    public String home() {
-        System.out.println("HomeController: Passing through...");
-        
-        logger.debug("Hej!");
-        return "patrik";
-    }
+
+	private PatrikService service;
+
+    @Autowired
+	//@Inject
+	public PatrikController(PatrikService patrikService)
+	{
+		this.service = patrikService;
+	}
+
+	@RequestMapping(
+	{ "/", "/patrik", "home" })
+	public String home(Map<String, Object> model)
+	{
+		System.out.println("HomeController: Passing through...");
+
+		logger.debug("Hej svejs!!");
+
+		Date now = new Date();
+
+		model.put("datum", now.toString());
+		model.put("echo", "just det");
+		return "patrik";
+	}
 }
